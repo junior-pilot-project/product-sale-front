84654a880508a6703fd4be294f8cfba91a9cd247
yunrap <dbsdpwl7@gmail.com>
2024-09-05 23:08:28 +0900

    feat. 탭기능 구현

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -60,79 +60,145 @@ const DetailPage = () => {
             </div>
           </div>
         </div>
-        <div className={`${styles.menubar}`}>
-          <Button>상품상세</Button>
-          <Button className={`${styles.whiteButton}`}>상품평</Button>
-          <Button>상품문의</Button>
-        </div>
-        <div className={`${styles.detailReview}`}>
-          <label>상품평</label>
-          <div className={`${styles.reviewStar}`}>
-            <img src={require('../assets/review_star.png')} alt="" />
-            <span>771</span>
+        <div>
+          <div className={`${styles.menubar}`}>
+            <a href="#section1" className={`${styles.menu1}`}>
+              상품상세
+            </a>
+            <a
+              href="#section2"
+              className={`${styles.menu2} ${styles.whiteButton}`}
+            >
+              상품평
+            </a>
+            <a href="#section3" className={`${styles.menu3}`}>
+              상품문의
+            </a>
           </div>
-          <div className={`${styles.reviewList}`}>
-            <div className={`${styles.reviewPeople}`}>
-              <img
-                src={require('../assets/people.png')}
-                alt=""
-                style={{
-                  width: '50px',
-                  height: '50px',
-                }}
-              />
-              <span>홍길동</span>
+          <div className={`${styles.sectionContent}`}>
+            <div id="section1" style={{ paddingTop: '70px' }}>
+              <label>상품상세</label>
+              <div>
+                상품상세상품상세상품상세상품상세상품상세상품상세상품상세상품상세상품상세상품상세
+                상품상세상품상세상품상세상품상세
+              </div>
             </div>
-            <div className={`${styles.date}`}>2024.08.22</div>
-            <div className={`${styles.imageArea}`}>
-              <img src={require('../assets/image.png')} alt="" />
-              <img src={require('../assets/image.png')} alt="" />
-              <img src={require('../assets/image.png')} alt="" />
-              <img src={require('../assets/image.png')} alt="" />
-              <img src={require('../assets/image.png')} alt="" />
-              <img src={require('../assets/image.png')} alt="" />
-              <img src={require('../assets/image.png')} alt="" />
+
+            <div id="section2" style={{ paddingTop: '70px' }}>
+              <label>상품평</label>
+              <div className={`${styles.reviewStar}`}>
+                <img src={require('../assets/review_star.png')} alt="" />
+                <span>771</span>
+              </div>
+              <div className={`${styles.reviewList}`}>
+                <div className={`${styles.reviewPeople}`}>
+                  <img
+                    src={require('../assets/people.png')}
+                    alt=""
+                    style={{
+                      width: '50px',
+                      height: '50px',
+                    }}
+                  />
+                  <span>홍길동</span>
+                </div>
+                <div className={`${styles.date}`}>2024.08.22</div>
+                <div className={`${styles.imageArea}`}>
+                  <img src={require('../assets/image.png')} alt="" />
+                  <img src={require('../assets/image.png')} alt="" />
+                  <img src={require('../assets/image.png')} alt="" />
+                  <img src={require('../assets/image.png')} alt="" />
+                  <img src={require('../assets/image.png')} alt="" />
+                  <img src={require('../assets/image.png')} alt="" />
+                  <img src={require('../assets/image.png')} alt="" />
+                </div>
+                <div className={`${styles.reviewContent}`}>
+                  <p>
+                    따뜻하고 좋아요 이거산이후로 이거만 입게되는거같네요 ㅎㅎㅎ
+                  </p>
+                </div>
+                <hr></hr>
+              </div>
+              <div className={`${styles.reviewList}`}>
+                <div className={`${styles.reviewPeople}`}>
+                  <img
+                    src={require('../assets/people.png')}
+                    alt=""
+                    style={{
+                      width: '50px',
+                      height: '50px',
+                    }}
+                  />
+                  <span>홍길동</span>
+                </div>
+                <div className={`${styles.date}`}>2024.08.22</div>
+                <div className={`${styles.reviewContent}`}>
+                  따뜻하고 좋아요 이거산이후로 이거만 입게되는거같네요 ㅎㅎㅎ
+                </div>
+                <hr></hr>
+              </div>
+              <div className={`${styles.reviewList}`}>
+                <div className={`${styles.reviewPeople}`}>
+                  <img
+                    src={require('../assets/people.png')}
+                    alt=""
+                    style={{
+                      width: '50px',
+                      height: '50px',
+                    }}
+                  />
+                  <span>홍길동</span>
+                </div>
+                <div className={`${styles.date}`}>2024.08.22</div>
+                <div className={`${styles.reviewContent}`}>
+                  따뜻하고 좋아요 이거산이후로 이거만 입게되는거같네요 ㅎㅎㅎ
+                </div>
+                <hr></hr>
+              </div>
+              <div className={`${styles.reviewList}`}>
+                <div className={`${styles.reviewPeople}`}>
+                  <img
+                    src={require('../assets/people.png')}
+                    alt=""
+                    style={{
+                      width: '50px',
+                      height: '50px',
+                    }}
+                  />
+                  <span>홍길동</span>
+                </div>
+                <div className={`${styles.date}`}>2024.08.22</div>
+                <div className={`${styles.reviewContent}`}>
+                  따뜻하고 좋아요 이거산이후로 이거만 입게되는거같네요 ㅎㅎㅎ
+                </div>
+                <hr></hr>
+              </div>
+              <div className={`${styles.reviewList}`}>
+                <div className={`${styles.reviewPeople}`}>
+                  <img
+                    src={require('../assets/people.png')}
+                    alt=""
+                    style={{
+                      width: '50px',
+                      height: '50px',
+                    }}
+                  />
+                  <span>홍길동</span>
+                </div>
+                <div className={`${styles.date}`}>2024.08.22</div>
+                <div className={`${styles.reviewContent}`}>
+                  따뜻하고 좋아요 이거산이후로 이거만 입게되는거같네요 ㅎㅎㅎ
+                </div>
+                <hr></hr>
+              </div>
             </div>
-            <div className={`${styles.reviewContent}`}>
-              <p>따뜻하고 좋아요 이거산이후로 이거만 입게되는거같네요 ㅎㅎㅎ</p>
+            <div id="section3" style={{ paddingTop: '70px' }}>
+              <label>상품문의</label>
+              <div>
+                상품문의상품문의상품문의상품문의상품문의상품문의상품문의상품문의상품문의상품문의상품문의상품문의
+                상품문의상품문의상품문의상품문의
+              </div>
             </div>
-            <hr></hr>
-          </div>
-          <div className={`${styles.reviewList}`}>
-            <div className={`${styles.reviewPeople}`}>
-              <img
-                src={require('../assets/people.png')}
-                alt=""
-                style={{
-                  width: '50px',
-                  height: '50px',
-                }}
-              />
-              <span>홍길동</span>
-            </div>
-            <div className={`${styles.date}`}>2024.08.22</div>
-            <div className={`${styles.reviewContent}`}>
-              따뜻하고 좋아요 이거산이후로 이거만 입게되는거같네요 ㅎㅎㅎ
-            </div>
-            <hr></hr>
-          </div>
-          <div className={`${styles.reviewList}`}>
-            <div className={`${styles.reviewPeople}`}>
-              <img
-                src={require('../assets/people.png')}
-                alt=""
-                style={{
-                  width: '50px',
-                  height: '50px',
-                }}
-              />
-              <span>홍길동</span>
-            </div>
-            <div className={`${styles.date}`}>2024.08.22</div>
-            <div className={`${styles.reviewContent}`}>
-              따뜻하고 좋아요 이거산이후로 이거만 입게되는거같네요 ㅎㅎㅎ
-            </div>
-            <hr></hr>
           </div>
         </div>
       </div>
