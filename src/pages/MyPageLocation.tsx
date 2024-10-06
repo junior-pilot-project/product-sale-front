f05605aae1d43f2e79b4ffcaaa985ddc237a63a6
yunrap <dbsdpwl7@gmail.com>
2024-10-07 00:55:42 +0900

    feat. 마이페이지 개인정보확인/수정 기능추가

--- src/pages/MyPageLocation.tsx
+++ src/pages/MyPageLocation.tsx
@@ -29,37 +29,39 @@ const MyPageLocation = () => {
       <div className={`${styles.myPage}`}>
         <div className={`${styles.label}`}>My쿠팡</div>
         <hr></hr>
-        <div className={`${styles.myPageContainer}`}>
+        <div className={`${styles.content}`}>
           <LeftTab></LeftTab>
           <div className={`${styles.contentRight}`}>
-            <div className={`${styles.label}`}>배송지 관리</div>
-            <div className={`${styles.container}`}>
-              <InputBoxArea
-                placeholder="경기도 부천시 금천로 334"
-                imgsrc={require('../assets/icon/icon_mail.png')}
-                onChangeValue={onChangeEvent}
-              ></InputBoxArea>
-              <div className={`${styles.defaultDelival}`}>
-                <input type="checkbox"></input>
-                <div className={`${styles.checkboxText}`}>
-                  기본배송지로 선택
+            <section className={`${styles.section1}`}>
+              <div className={`${styles.textBlue}`}>배송지 관리</div>
+              <div className={`${styles.container}`}>
+                <InputBoxArea
+                  placeholder="경기도 부천시 금천로 334"
+                  imgsrc={require('../assets/icon/icon_mail.png')}
+                  onChangeValue={onChangeEvent}
+                ></InputBoxArea>
+                <div className={`${styles.defaultDelival}`}>
+                  <input type="checkbox"></input>
+                  <div className={`${styles.checkboxText}`}>
+                    기본배송지로 선택
+                  </div>
+                </div>
+                <div className={`${styles.bottom} `}>
+                  <button
+                    className={`button ${styles.buttonFillWhite} `}
+                    onClick={() => navigate('/myPageLocationList')}
+                  >
+                    취소하기
+                  </button>
+                  <button
+                    className={`button ${styles.buttonFillBlue} `}
+                    onClick={onSaveAddress}
+                  >
+                    저장
+                  </button>
                 </div>
               </div>
-              <div className={`${styles.bottom} `}>
-                <button
-                  className={`button ${styles.buttonFillWhite} `}
-                  onClick={() => navigate('/myPageLocationList')}
-                >
-                  취소하기
-                </button>
-                <button
-                  className={`button ${styles.buttonFillBlue} `}
-                  onClick={onSaveAddress}
-                >
-                  저장
-                </button>
-              </div>
-            </div>
+            </section>
           </div>
         </div>
       </div>
