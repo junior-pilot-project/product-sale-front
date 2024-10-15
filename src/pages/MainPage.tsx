82a5a56de06c75eea970e35947c34d21f476f53a
yunrap <dbsdpwl7@gmail.com>
2024-10-15 14:41:16 +0900

    feat. 메인페이지 디자인수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -10,111 +10,54 @@ const MainPage = () => {
         <img src={require('../assets/banner.jpg')} alt="오늘단하루 할인" />
       </div>
       <div className={`${styles.mainContainer}`}>
-        <h2 className={`${styles.categoryProductLabel}`}>
-          HOT! TREND
-          <br />
-          카테고리별
-          <span className={`${styles.textBlue}`}> 추천 광고상품</span>
-        </h2>
+        <div>
+          <h2 className={`${styles.label}`}>
+            HOT! TREND
+            <br />
+            카테고리별
+            <span className={`${styles.textBlue}`}> 추천 광고상품</span>
+          </h2>
+        </div>
         <section className={`${styles.productSection}`}>
-          <span className={`${styles.textRed}`}>여성패션</span>
-          <ul className={`${styles.productList}`}>
-            <li>
-              <Link to="/detail" className={`${styles.productUnit}`}>
-                <img
-                  src={require('../assets/cloth1.png')}
-                  width={'160px'}
-                  height={'150px'}
-                  alt=""
-                ></img>
-                <span className={`${styles.productName}`}>
-                  [GRA/엑설런트] 14k.p 캐럿미 파인주얼리 모이사나이트 랩 러블리
-                  플라워 팔찌
-                </span>
-                <span>14000원</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/detail" className={`${styles.productUnit}`}>
-                <img
-                  src={require('../assets/cloth1.png')}
-                  width={'160px'}
-                  height={'150px'}
-                  alt=""
-                ></img>
-                <span className={`${styles.productName}`}>
-                  [GRA/엑설런트] 14k.p 캐럿미 파인주얼리 모이사나이트 랩 러블리
-                  플라워 팔찌
-                </span>
-                <span>14000원</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/detail" className={`${styles.productUnit}`}>
-                <img
-                  src={require('../assets/cloth1.png')}
-                  width={'160px'}
-                  height={'150px'}
-                  alt=""
-                ></img>
-                <span className={`${styles.productName}`}>
-                  [GRA/엑설런트] 14k.p 캐럿미 파인주얼리 모이사나이트 랩 러블리
-                  플라워 팔찌
-                </span>
-                <span>14000원</span>
-              </Link>
-            </li>
-          </ul>
-        </section>
-        <section className={`${styles.productSection}`}>
-          <span className={`${styles.textBlue}`}>남성패션</span>
-          <ul className={`${styles.productList}`}>
-            <li>
-              <Link to="/detail" className={`${styles.productUnit}`}>
-                <img
-                  src={require('../assets/cloth1.png')}
-                  width={'160px'}
-                  height={'150px'}
-                  alt=""
-                ></img>
-                <span className={`${styles.productName}`}>
-                  [GRA/엑설런트] 14k.p 캐럿미 파인주얼리 모이사나이트 랩 러블리
-                  플라워 팔찌
-                </span>
-                <span>14000원</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/detail" className={`${styles.productUnit}`}>
-                <img
-                  src={require('../assets/cloth1.png')}
-                  width={'160px'}
-                  height={'150px'}
-                  alt=""
-                ></img>
-                <span className={`${styles.productName}`}>
-                  [GRA/엑설런트] 14k.p 캐럿미 파인주얼리 모이사나이트 랩 러블리
-                  플라워 팔찌
-                </span>
-                <span>14000원</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/detail" className={`${styles.productUnit}`}>
-                <img
-                  src={require('../assets/cloth1.png')}
-                  width={'160px'}
-                  height={'150px'}
-                  alt=""
-                ></img>
-                <span className={`${styles.productName}`}>
-                  [GRA/엑설런트] 14k.p 캐럿미 파인주얼리 모이사나이트 랩 러블리
-                  플라워 팔찌
-                </span>
-                <span>14000원</span>
-              </Link>
-            </li>
-          </ul>
+          <div className={`${styles.productCategory}`}>여성패션</div>
+          <img src={require('../assets/product.png')} alt=""></img>
+          <div className={`${styles.productList}`}>
+            <Link to="/detail" className={`${styles.productBox}`}>
+              <div>
+                <img src={require('../assets/cloth1.png')} alt=""></img>
+                <div className={`${styles.productName}`}>roka 군인 옷</div>
+                <div className={`${styles.productPrice}`}>14,000원</div>
+              </div>
+            </Link>
+            <Link to="/detail" className={`${styles.productBox}`}>
+              <div>
+                <img src={require('../assets/cloth1.png')} alt=""></img>
+                <div className={`${styles.productName}`}>roka 군인 옷</div>
+                <div className={`${styles.productPrice}`}>14,000원</div>
+              </div>
+            </Link>
+            <Link to="/detail" className={`${styles.productBox}`}>
+              <div>
+                <img src={require('../assets/cloth1.png')} alt=""></img>
+                <div className={`${styles.productName}`}>roka 군인 옷</div>
+                <div className={`${styles.productPrice}`}>14,000원</div>
+              </div>
+            </Link>
+            <Link to="/detail" className={`${styles.productBox}`}>
+              <div>
+                <img src={require('../assets/cloth1.png')} alt=""></img>
+                <div className={`${styles.productName}`}>roka 군인 옷</div>
+                <div className={`${styles.productPrice}`}>14,000원</div>
+              </div>
+            </Link>
+            <Link to="/detail" className={`${styles.productBox}`}>
+              <div>
+                <img src={require('../assets/cloth1.png')} alt=""></img>
+                <div className={`${styles.productName}`}>roka 군인 옷</div>
+                <div className={`${styles.productPrice}`}>14,000원</div>
+              </div>
+            </Link>
+          </div>
         </section>
       </div>
     </>
